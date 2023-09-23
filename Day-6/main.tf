c129a763e00aef40a453ac2f3dd210c24e854111
vivekzambre <103733849+vivekzambre05@users.noreply.github.com>
2023-09-23 08:57:25 +0530

    Update main.tf

--- Day-6/main.tf
+++ Day-6/main.tf
@@ -8,17 +8,10 @@ variable "ami" {
 
 variable "instance_type" {
   description = "value"
-  type = map(string)
-
-  default = {
-    "dev" = "t2.micro"
-    "stage" = "t2.medium"
-    "prod" = "t2.xlarge"
-  }
 }
 
 module "ec2_instance" {
   source = "./modules/ec2_instance"
   ami = var.ami
-  instance_type = lookup(var.instance_type, terraform.workspace, "t2.micro")
+  instance_type = var.instance_type
 }
